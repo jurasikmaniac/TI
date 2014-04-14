c17d2c62a0023e6acc6b7a4c531fd6a1e4f782c3
Yurii Loschakov <jurasikmaniac>
2014-04-14 17:24:04 +0700

    2

--- Zadacha_1/Zadacha_1.cpp
+++ Zadacha_1/Zadacha_1.cpp
@@ -4,76 +4,157 @@
 #include "stdafx.h"
 
 #include <cmath>
-#include <cctype>
 #include <cstdio>
-#include <cstdlib>
+#include <ctype.h> 
+#include <clocale>
 #include <Windows.h>
 
 #pragma warning (once : 4996)
-
-float entropy_1 (int *mass);
-float entropy_2 (int *mass);
+#define AlfabetN 256	
 
 //B Visual studio 2012 нет поддержки log2
 inline float log2(float n) {
   return log(n) / log(2.0);
 }
 
-//Меню программы, type - задает нужные пункты меню.
-inline unsigned char Menu(unsigned char type)
-{
-	return 0;
-} 
+
 
 //Предварительные настройки приложения
 inline void SetApp(void)
 {
+	//setlocale( LC_ALL, "ru_RU.cp1251" );
+
 	SetConsoleCP(1251);					// Русская локаль
 	SetConsoleOutputCP(1251);
 }
 
 //Оценка энтропии, одиночные символы
-float entropy_1 (int *mass)
+float entropy_1 (char *fn)
 {
+	FILE *fi;
+	float freq[AlfabetN] = {0};
+	int ch=EOF;
+	long total=0;
+	puts("Файл:");	
+	puts(fn);
+	fi = fopen(fn, "r");
+	if(fi == NULL)
+    {
+        puts("Такой файл не найден!");
+		system("Pause");
+        return -1;
+    }
+	
+	_locale_t m_locale;
+	
+	 m_locale =  _get_current_locale();
+	 wprintf(L"%ls sdsd\n",m_locale->locinfo->locale_name);
+	while((ch = fgetc(fi)) != EOF)
+    {
+		printf("%c", ch);
+        if(_isalpha_l(ch,m_locale))
+        {
+            ch = _tolower_l(ch,m_locale);
+            //if(ch == toascii('ё')) ch = toascii('е');
+            freq[ch]++;
+            total++;            
+        }
+        else 
+        {
+			if (ch == ' ')
+			{
+				freq[ch]++;
+				total++;            
+			}else
+			{
+				ch = '.'; // всё остальное
+				freq[ch]++;
+				total++;            
+			}
+        }
+ 
+    }
+	
+	for (int i = 0; i < AlfabetN; i++)
+	{
+		
+		if (freq[i]!=0)
+		{
+			freq[i] = freq[i]/total;
+			printf("P[%c] = %1.3f\n",i,freq[i]);
+		}
+	}
+	printf("я = %c", _tolower_l( 'Я',m_locale));
+	
+	_free_locale(m_locale);
+	system("PAUSE");
 	return 0;
 }
 
 //Оценка энтропии, пары символов
-float entropy_2 (int *mass)
+float entropy_2 (char *fl)
 {
+	puts("Файл:");	
+	puts(fl);
 	return 0;
 }
 
+//Меню программы, type - задает нужные пункты меню.
+inline wchar_t Menu(unsigned char type)
+{
+	wchar_t c;
+	switch (type)
+	{
+		case 1:	system("CLS");
+				puts("########################");
+				puts("1. Ввести имя файла");
+				puts("2. Оценка энтропии, одиночные символы");
+				puts("3. Оценка энтропии, пары символов");
+				puts("4. Выход из программы");
+				puts("\n########################");
+				puts("Выбрать пункт меню №: ");
+				c=getchar();
+				fflush(stdin);
+				return c;
+		default:puts("Err, нет такого меню");
+				system("PAUSE");
+				break;
+	}
+	
+	return 0;
+} 
 
 
 
 
-
-#define AlfabetN 256					// Количество символов в алфавите
+				// Количество символов в алфавите
 int _tmain(int argc, _TCHAR* argv[])
 {
-	float freq[AlfabetN] = {0};
-	float freq2[AlfabetN*AlfabetN] = {0};
-	FILE *fi;
-	float H=0;
-
-	SetApp ( );
-	Menu ( 1 );
 	
-	freq[0]=3.0;
-	freq[1]=1.0;
-	freq[2]=4.0;
-	for (int i = 0; i < 3; i++)
+	float freq2[AlfabetN][AlfabetN] = {0};
+	char FILENAME[200];
+
+
+	SetApp ();
+	while (1)
 	{
-		H += (-1)* (freq[i]/8.0)*log2(freq[i]/8.0);
-		printf("P = %f\n", freq[i]/8.0);
-		printf("log = %f\n", log2(freq[i]/8.0));
+		switch (Menu(1))
+		{
+			case '1': fflush(stdin); system("DIR /a:-d"); puts("Первая буква Большая!\n"); gets(FILENAME);
+				break;
+			case '2': entropy_1( FILENAME);break;
+			case '3': entropy_1( FILENAME);break;
+			case '4': return 0;
+			default:break;
+		}
 	}
-	printf("H = %6.3f", H);
+	
+	
+	
 	/*Вывод кодов ASCII, для проверки правильности их определения
 	for (int i = 0; i < AlfabetN; i++)
 	{
-		printf("P[%c-%3d] = %4d\n",i,i,P1[i]);
+		puts("P[%c-%3d] = %4d\n",i,i,P1[i]);
 	}*/
 	
 	system("PAUSE");
